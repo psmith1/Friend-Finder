0fa02a8721aa21bfa0caf3848a01ad41b980207c
Peter Smith <petergbsmith@gmail.com>
2019-12-20 19:43:03 -0600

    fixing friendCheck Fn

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,101 +1,36 @@
+/* eslint-disable no-console */
 var friends = [
-    {
-        "name": "Richard",
-        "photo": "https://media.giphy.com/media/VhKR7WDlHyBhje9DMr/giphy.gif",
-        "scores": [
-            "3",
-            "5",
-            "4",
-            "1",
-            "5",
-            "3",
-            "5",
-            "4",
-            "5",
-            "5"
-        ]
-    },
-    {
-        "name": "Jared",
-        "photo": "https://i.pinimg.com/originals/0a/a4/cd/0aa4cd14c7d7a4a07e71ba82c245632d.gif",
-        "scores": [
-            "4",
-            "3",
-            "5",
-            "5",
-            "2",
-            "5",
-            "5",
-            "2",
-            "1",
-            "5"
-        ]
-    },
-    {
-        "name": "Gilfoyle",
-        "photo": "https://media.giphy.com/media/IfgkVFVOEPqWUoyOVc/giphy.gif",
-        "scores": [
-            "2",
-            "5",
-            "3",
-            "1",
-            "4",
-            "1",
-            "4",
-            "2",
-            "4",
-            "3"
-        ] 
-    },
-    {
-        "name": "Dinesh",
-        "photo": "https://media3.giphy.com/media/xUA7aYjnWxBv8TM2s0/source.gif",
-        "scores": [
-            "2",
-            "2",
-            "3",
-            "1",
-            "4",
-            "3",
-            "3",
-            "4",
-            "4",
-            "3"
-        ]
-    },
-    {
-        "name": "Monica",
-        "photo": "https://media.giphy.com/media/iDCfYMNfnHpYEdn6nJ/giphy.gif",
-        "scores": [
-            "5",
-            "3",
-            "3",
-            "5",
-            "5",
-            "2",
-            "4",
-            "3",
-            "1",
-            "3"
-        ]
-    },
-    {
-        "name": "Jian Yang",
-        "photo": "https://media1.giphy.com/media/MuE0xWbEohUrxbm77r/giphy.gif",
-        "scores":[
-            "4",
-            "2",
-            "1",
-            "5",
-            "1",
-            "3",
-            "1",
-            "1",
-            "1",
-            "2"
-        ]
-    },
-]
+        {
+                name: 'Richard',
+                photo: 'https://media.giphy.com/media/VhKR7WDlHyBhje9DMr/giphy.gif',
+                scores: ['3', '5', '4', '1', '5', '3', '5', '4', '5', '5'],
+        },
+        {
+                name: 'Jared',
+                photo: 'https://i.pinimg.com/originals/0a/a4/cd/0aa4cd14c7d7a4a07e71ba82c245632d.gif',
+                scores: ['4', '3', '5', '5', '2', '5', '5', '2', '1'],
+        },
+        {
+                name: 'Gilfoyle',
+                photo: 'https://media.giphy.com/media/IfgkVFVOEPqWUoyOVc/giphy.gif',
+                scores: ['2', '5', '3', '1', '4', '1', '4', '2', '4'],
+        },
+        {
+                name: 'Dinesh',
+                photo: 'https://media3.giphy.com/media/xUA7aYjnWxBv8TM2s0/source.gif',
+                scores: ['2', '2', '3', '1', '4', '3', '3', '4', '4', '3'],
+        },
+        {
+                name: 'Monica',
+                photo: 'https://media.giphy.com/media/iDCfYMNfnHpYEdn6nJ/giphy.gif',
+                scores: ['5', '3', '3', '5', '5', '2', '4', '3', '1', '3'],
+        },
+        {
+                name: 'Jian Yang',
+                photo: 'https://media1.giphy.com/media/MuE0xWbEohUrxbm77r/giphy.gif',
+                scores: ['4', '2', '1', '5', '1', '3', '1', '1', '1', '2'],
+        },
+];
 
 console.log(friends);
 module.exports = friends;
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,56 +1,68 @@
-var friends = require("../data/friends.js");
+/* eslint-disable no-plusplus */
+/* eslint-disable vars-on-top */
+/* eslint-disable no-console */
+var friends = require('../data/friends.js');
 
-module.exports = function (app) {
-    
-    app.get("/api/friends", function(req, res) {
-        res.json(friends);
-    });
+module.exports = function(app) {
+        app.get('/api/friends', function(req, res) {
+                res.json(friends);
+        });
 
-    app.post("/api/friends", function(req, res) {
+        app.post('/api/friends', function(req, res) {
+                var bestMatch = {
+                        name: '',
+                        photo: '',
+                        friendDifference: 1000,
+                };
 
-        var bestMatch = {
-            name: "",
-            photo: "",
-            friendDifference: 1000
-        };
+                console.log(req.body);
 
-        console.log(req.body);
+                // Get the results of the user's survey and POST and parse it
+                var userData = req.body;
+                var userScores = userData.scores;
 
-        // Get the results of the user's survey and POST and parse it
-        var userData = req.body;
-        var userScores = userData.scores;
+                console.log(userScores);
 
-        console.log(userScores);
+                // This variable will be used to calculate the difference between the user's scores and the scores of each potential friend in the database
+                var totalDifference = 0;
 
-        // This variable will be used to calculate the difference between the user's scores and the scores of each potential friend in the database
-        var totalDifference = 0;
+                //  Loop through all the friend possibilitites in the database
+                const friendCheck = function() {
+                        for (var i = 0; i < friends.length; i++) {
+                                console.log(friends[i]);
+                                totalDifference = 0;
 
-        //  Loop through all the friend possibilitites in the database
-        for (var i = 0; i < friends.length; i++) {
+                                // Loop through all the scores of each friend in the database
+                                for (var j = 0; j < friends[i].scores[j]; j++) {
+                                        // Calculate the difference between the scores and sum them into the totalDifference
+                                        totalDifference += Math.abs(
+                                                parseInt(userScores[j]) - parseInt(friends[i].scores[j])
+                                        );
 
-            console.log(friends[i]);
-            totalDifference = 0;
+                                        // If the sum of differences is less than the differences of the current best match
+                                        if (
+                                                `userData.name.toLowerCase() !== bestMatch.name.toLowerCase()` ||
+                                                `totalDifference <= bestMatch.friendDifference`
+                                        ) {
+                                                // Reset the best match to be the new friend
+                                                bestMatch.name = friends[i].name;
+                                                console.log(bestMatch.name);
+                                                bestMatch.photo = friends[i].photo;
+                                                console.log(bestMatch.photo);
+                                                bestMatch.friendDifference = totalDifference;
+                                                console.log(bestMatch.friendDifference);
+                                                console.log(totalDifference);
+                                        } else {
+                                                friendCheck();
+                                        }
+                                }
+                        }
+                };
+                friendCheck();
+                // Save the user's data into the database
+                friends.push(userData);
 
-            // Loop through all the scores of each friend in the database
-            for (var j = 0; j < friends[i].scores[j]; j++) {
-
-                // Calculate the difference between the scores and sum them into the totalDifference
-                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
-
-                // If the sum of differences is less than the differences of the current best match
-                if (totalDifference <= bestMatch.friendDifference) {
-
-                // Reset the best match to be the new friend
-                bestMatch.name = friends[i].name;
-                bestMatch.photo = friends[i].photo;
-                bestMatch.friendDifference = totalDifference;
-                }
-            }
-        }
-        // Save the user's data into the database
-        friends.push(userData);
-
-        // Return JSON with the user's best match
-        res.json(bestMatch);
-    });
-}
+                // Return JSON with the user's best match
+                res.json(bestMatch);
+        });
+};
